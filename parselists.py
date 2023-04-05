2117ffa0dc40b8e05edf27b994f90b01d546305c
Dan Mick <danmick@gmail.com>
2023-04-05 03:56:09 +0000

    Skip contents comparison in debug mode

--- parselists.py
+++ parselists.py
@@ -13,27 +13,29 @@ else:
 EXPORTED_CSV='https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-Wh4Xd7n7wuBb0gnOpmoO2GFwTpvXEK0fcXd1dwF8GOrhV7z8vQXjGPKE5Is3UgMNeDOGSqwGmHR2/pub?gid=0&single=true&output=csv'
 musicians = requests.get(EXPORTED_CSV)
 
-
-try:
-    with open('all_musicians.csv', 'rb') as f:
-        h = hashlib.sha256()
-        h.update(f.read())
-        old_checksum = h.digest()
-    h = hashlib.sha256()
-    h.update(musicians.content)
-    new_checksum = h.digest()
-    if old_checksum == new_checksum:
-        if debug:
-            print('Musicians list unchanged, exiting') 
-        exit(1)
-except FileNotFoundError as e:
-    print('Ignoring', e)
-    pass
+# don't do contents comparison in debug mode
+if not debug:
+    try:
+        with open('all_musicians.csv', 'rb') as f:
+            h = hashlib.sha256()
+            h.update(f.read())
+            old_checksum = h.digest()
+            h = hashlib.sha256()
+            h.update(musicians.content)
+            new_checksum = h.digest()
+            if old_checksum == new_checksum:
+                if debug:
+                    print('Musicians list unchanged, exiting') 
+                exit(1)
+    except FileNotFoundError as e:
+        print('Ignoring', e)
+        pass
 
 with open('all_musicians.csv', 'w') as f:
     f.write(musicians.text)
 
 all_musicians = csv.reader(musicians.iter_lines(decode_unicode=True))
+
 out_file = None
 mlistname = ''
 mlist = list()
