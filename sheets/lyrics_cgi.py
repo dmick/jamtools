d8e6f05375a2af92244499abcf111ad306b13adf
Dan Mick <danmick@gmail.com>
2024-11-21 20:04:47 +0000

    lyrics_cgi.py: use fetch_lyrics.py to provide a web lyrics gen
    This should probably be recoded to not invoke external programs.
    The CGI started as bash, but now that it's Python there's no reason
    not to compose these of Python functions.
    
    Signed-off-by: Dan Mick <danmick@gmail.com>

---
+++ sheets/lyrics_cgi.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import subprocess
+from subprocess import PIPE
+from urllib.parse import parse_qs
+
+TMPFILE='/tmp/setlist.tmp'
+LYRCMD='/home/dmick/src/sheets/fetch_lyrics.py %s' 
+LISTCMD='/home/dmick/src/sheets/fetch_sets.py -l -d %s'
+
+def run_command(args):
+    # print(f'{args=}', file=open('/tmp/tmp', 'w'))
+    if isinstance(args, str):
+        args = args.split(' ')
+    p = subprocess.Popen(args, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    # print(f'{out=} {err=}', file=open('/tmp/output', 'w'))
+    return p.returncode, out, err
+
+def print_form():
+    print(f'''Content-Type: text/html
+
+<html>
+<head>
+<style>
+  label,button {{display: block}}
+</style>
+</head>
+<body>
+<form method=GET action="{os.environ['SCRIPT_NAME']}">
+<label for="date">Date (Use Last Call setlist from this date)</label>
+<input type="date" id="date" name="date">
+<label for="setlist">Or enter a setlist here (lines of song,artist) </label>
+<textarea id="setlist" name="setlist" rows="25" cols="40">One,U2\nTwo Hearts Beat As One,U2</textarea>
+<label for="html">Output HTML</label>
+<input type=checkbox id="html" name="html" value="true">
+<button type="reset">Reset form</button>
+<button type="submit">Go</button>
+</form>
+</body>
+</html>''')
+
+
+HEADER='<html><body>'
+FOOTER='</body></html>'
+
+SCROLL_SCRIPT='''
+<body onkeydown="scrollfunc(event)">
+<script>
+function scrollfunc(e) {
+        let scrolldist = window.innerHeight / 2;
+        if (e.key == "PageDown") {
+                window.scrollBy(0, scrolldist);
+                e.preventDefault();
+        } else if (e.key == "PageUp") {
+                window.scrollBy(0, -scrolldist);
+                e.preventDefault();
+        }
+}
+</script>'''
+
+CSS='''
+<style>
+html {
+        scroll-behavior: smooth;
+}
+p {
+        margin: 0px 10px 0px 0px;
+        color:white;
+        background-color: black;
+        font-family: sans-serif;
+        font-size: 65px;
+        font-weight: 500;
+        font-style: sans;
+        letter-spacing: 0px;
+        text-align: center;
+}
+</style>'''
+
+
+def do_fetch(date, setlist=None, html=False):
+    if (not date) and setlist:
+        with open(TMPFILE, 'w') as setlistfile:
+            # handle firstline has 'artist' but not 'song,artist'
+            # or firstline has no 'artist' (assume no fields line at all)
+            firstline = setlist.split('\n')[0]
+            if (('artist' in firstline) and (firstline != 'song,artist')):
+                setlist = 'song,artist\n' + '\n'.join(setlist.split('\n')[1:])
+            elif ('artist' not in firstline):
+                setlist = 'song,artist\n' + setlist 
+            setlistfile.write(setlist)
+    else:
+        retcode, out, err = run_command(LISTCMD % date)
+        if retcode:
+            print('Content-Type: text/plain\n')
+            print(f'ERROR:{err.decode()}')
+            return
+        with open(TMPFILE, 'w') as setlistfile:
+            setlistfile.write(out.decode())
+    retcode, out, err = run_command(LYRCMD % TMPFILE)
+    if retcode:
+        print('Content-Type: text/plain\n')
+        print(f'ERROR:{err.decode()}')
+        return 1
+
+    if out:
+        if html:
+            print('Content-Type: text/html\n')
+            print(HEADER, CSS, SCROLL_SCRIPT)
+            for l in out.decode().split('\n'):
+                l = l.strip()
+                if not len(l):
+                    l = '&nbsp'
+                print(f'<p>{l}</p>')
+            print(FOOTER)
+        else:
+            print('Content-Type: text/plain\n')
+            print(f'{out.decode()}')
+    return 0
+
+
+def main():
+    qs = os.environ.get('QUERY_STRING', '')
+    qd = parse_qs(qs)
+
+    date = qd.get('date')
+    setlist = qd.get('setlist')
+    html = qd.get('html')
+    if date:
+        date = date[0]
+    if setlist:
+        setlist = setlist[0]
+
+    if not date and not setlist:
+        print_form()
+        return 0
+
+    return do_fetch(date, setlist, html)
+
+if __name__ == "__main__":
+    sys.exit(main())
